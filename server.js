98d4bf37c958465df125b79febb521a8349a4ca3
Gibs-09 <safaricom2547@gmail.com>
2026-06-30 14:02:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Home page
+const path = require("path");
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to My First Render App!</h1><p>Your app is working.</p>");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Dynamic link
 app.get("/a/:code", (req, res) => {
-  const code = req.params.code;
-
-  res.send(`
-    <h1>Welcome!</h1>
-    <p>Your referral code is: <strong>${code}</strong></p>
-  `);
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
